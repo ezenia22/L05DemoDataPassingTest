ad8a51f7a95cd30f464844527cd0bd65945f029e
20011155 <20011155@myrp.edu.sg>
2021-05-25 14:54:39 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20011155/demodatapassingtest/MainActivity.java
@@ -0,0 +1,54 @@
+package sg.edu.rp.c346.id20011155.demodatapassingtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Event handling Step 1
+    Button btnPassInteger;
+    Button btnPassChar;
+    TextView txtPassDouble;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Event handling Step 2
+        btnPassInteger = findViewById(R.id.buttonPassInteger);
+        btnPassChar = findViewById(R.id.buttonPassChar);
+        txtPassDouble = findViewById(R.id.textPassDouble);
+
+        //Event handling Step 3
+        btnPassInteger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("value", 1);
+                startActivity(intent);
+            }
+        });
+        btnPassChar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
+                intent.putExtra("value", 'a');
+                startActivity(intent);
+            }
+        });
+        txtPassDouble.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FourthActivity.class);
+                intent.putExtra("value", 99.99);
+                startActivity(intent);
+            }
+        });
+    }
+}
